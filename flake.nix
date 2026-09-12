c2d73a2112be85e9813526796a46e941792fb3ec
6ix <101804748+Dymon1403@users.noreply.github.com>
2026-09-12 14:28:56 +0300

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
         {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
-          home-manager.users.aptivace = import ./home.nix;
+          home-manager.users.dmitrj = import ./home.nix;
         }
       ];
     };
